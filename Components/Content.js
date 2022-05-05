1062ea8438a924b186d802117a9f40b7b5ac50e4
2Kelvin <kvin.mwangi254@gmail.com>
2022-05-05 09:41:35 +0300

    mapped over array & displayed items in Content component

--- Components/Content.js
+++ Components/Content.js
@@ -1,26 +1,37 @@
-const Content = () => {
-    const handlePlanetChange = () => {
-        const planets = ["Mars", "Earth", "Venus", "Jupiter"];
-        const planetIndex = Math.floor(Math.random() * 4);
-        return planets[planetIndex];
-    };
+import { useState } from "react";
 
-    const handleClick = () => console.log("You clicked me");
-    const handleNameClick = planet => console.log(`You clicked ${planet}`);
-    const handleEventClick = e => console.log(e.target.innerText);
-    const handleDoubleClicks = () => console.log("You double clicked me, yes?");
+const Content = () => {
+    const [items, setItems] = useState(
+        [
+            {
+                id: 1,
+                checked: false,
+                item:"One half pound bag of Cocoa Covered Almonds Unsalted"
+            },
+            {
+                id: 2,
+                checked: false,
+                item:"Item 2"
+            },
+            {
+                id: 3,
+                checked: false,
+                item:"Item 3"
+            }
+        ]
+    );
 
     return (
         <main>
-            <p onDoubleClick={handleDoubleClicks}>
-            Welcome to {handlePlanetChange()}
-            </p>
-            <br></br>
-            <button onClick={handleClick}>Click Me</button>
-            <br></br>
-            <button onClick={() => handleNameClick("Venus")}>Name Planet</button>
-            <br></br>
-            <button onClick={(e) => handleEventClick(e)}>So Eventful</button>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked={item.checked} />
+                        <label>{item.item}</label>
+                        <button>Delete</button>
+                    </li>
+                ))}
+           </ul>
         </main>
     );
 }
--- Components/Header.js
+++ Components/Header.js
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <header>
-            <h1>Hello World</h1>
+            <h1>The Checklist</h1>
         </header>
     );
 }
